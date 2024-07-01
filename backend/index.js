6d4ff4426f4d1557dc94d3dd29fb2b840f56651e
avvrt <113913079+Corindpz@users.noreply.github.com>
2024-07-01 08:52:55 +0100

    user info

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,31 @@
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
-app.use(cors({
-    origin: 'http://localhost:8080',
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: "http://localhost:8080",
+    credentials: true,
+  })
+);
 
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use("/assets", express.static(path.join(__dirname, "assets")));
 
-require('./src/routes/users.route')(app);
-require('./src/routes/friendship.route')(app);
-require('./src/routes/topic.route')(app);
-require('./src/routes/categories.route')(app);
-require('./src/routes/messagerie.route')(app);
+require("./src/routes/users.route")(app);
+require("./src/routes/friendship.route")(app);
+require("./src/routes/topic.route")(app);
+require("./src/routes/categories.route")(app);
+require("./src/routes/messagerie.route")(app);
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
